c41e33c57ac9adf9b8c64317d2d2b46a0c672d0c
NSCorreo <nscorreo@gmail.com>
2021-10-31 17:26:37 -0400

    Update LAS_and_Graphics.py

--- LAS_and_Graphics.py
+++ LAS_and_Graphics.py
@@ -28,7 +28,7 @@ def main():
 
 def welcome():
     
-    st.title('Image Processing using Streamlit')
+    st.title('LAS Processing and Graphics')
     
     st.subheader('Esta es una simple aplicación que muestra como utilizar la librería WELLY en Phyton,  par efectuar la lectura'
              + ' de archivos LAS y generar las gráficas  correspondientes. ')
